131e45148b1651b610f7bb6d84e5fd8dc93000ca
AspenTheLeaf <brooklynnwood@eagles.acs-k12.org>
2026-02-03 19:01:31 -0900

    Removed variables tied to the limitswitch.

--- src/main/java/frc/robot/subsystems/climber/ClimberIO.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIO.java
@@ -5,8 +5,6 @@ import org.littletonrobotics.junction.AutoLog;
 public interface ClimberIO {
   @AutoLog
   public static class ClimberIOInputs {
-    public boolean climberDown = false;
-    public boolean climberUp = false;
     public double motorPosition = 0.0;
     public double motorVoltage = 0;
     public double motorCurrent = 0;
