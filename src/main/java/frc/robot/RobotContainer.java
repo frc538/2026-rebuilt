a768e5f1431cb80faf599c4438e919a50fdf0eec
Nick Patrick <npatrick04@gmail.com>
2026-02-21 11:08:29 -0600

    Update formatting and libraries (#15)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -89,8 +89,8 @@ public class RobotContainer {
                 drive::addVisionMeasurement,
                 new VisionIOLimelight(camera0Name, drive::getRotation),
                 new VisionIOLimelight(camera1Name, drive::getRotation));
-                //new VisionIOLimelight(camera2Name, drive::getRotation),
-                //new VisionIOLimelight(camera3Name, drive::getRotation));
+        // new VisionIOLimelight(camera2Name, drive::getRotation),
+        // new VisionIOLimelight(camera3Name, drive::getRotation));
         hopper =
             new Hopper(
                 new HopperIOSparkMax(Constants.Hopper.FeedCanId, Constants.Hopper.SpindexCanId));
@@ -116,8 +116,8 @@ public class RobotContainer {
                 drive::addVisionMeasurement,
                 new VisionIOPhotonVisionSim(camera0Name, robotToCamera0, drive::getPose),
                 new VisionIOPhotonVisionSim(camera1Name, robotToCamera1, drive::getPose));
-                //new VisionIOPhotonVisionSim(camera2Name, robotToCamera2, drive::getPose),
-                //new VisionIOPhotonVisionSim(camera3Name, robotToCamera3, drive::getPose));
+        // new VisionIOPhotonVisionSim(camera2Name, robotToCamera2, drive::getPose),
+        // new VisionIOPhotonVisionSim(camera3Name, robotToCamera3, drive::getPose));
         hopper = new Hopper(new HopperIO() {});
         intake = new Intake(new IntakeIOSim(Constants.Intake.MovMotorCanId) {});
         climberSubsystem = new ClimberSubsystem(new ClimberIO() {});
