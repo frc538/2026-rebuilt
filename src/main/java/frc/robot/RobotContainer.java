d94b07391376caedf23eda9d778f09127c32aff9
Rocketdudes4442 <carl.stelly@outlook.com>
2026-02-21 18:39:26 -0600

    merged the code that made the intake stuff actually move

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,14 +7,37 @@
 
 package frc.robot;
 
-import static frc.robot.subsystems.vision.VisionConstants.*;
-
+import com.pathplanner.lib.auto.AutoBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.commands.DriveCommands;
+import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.Intake.Intake;
 import frc.robot.subsystems.Intake.IntakeIO;
 import frc.robot.subsystems.Intake.IntakeIOSpark;
+import frc.robot.subsystems.climber.ClimberIO;
+import frc.robot.subsystems.climber.ClimberIOSparkMax;
+import frc.robot.subsystems.climber.ClimberSubsystem;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.drive.GyroIO;
+import frc.robot.subsystems.drive.GyroIOPigeon2;
+import frc.robot.subsystems.drive.ModuleIO;
+import frc.robot.subsystems.drive.ModuleIOSim;
+import frc.robot.subsystems.drive.ModuleIOTalonFX;
+import frc.robot.subsystems.hopper.Hopper;
+import frc.robot.subsystems.hopper.HopperIO;
+import frc.robot.subsystems.hopper.HopperIOSparkMax;
+import frc.robot.subsystems.vision.Vision;
+import frc.robot.subsystems.vision.VisionIO;
+import frc.robot.subsystems.vision.VisionIOLimelight;
+import frc.robot.subsystems.vision.VisionIOPhotonVisionSim;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -24,14 +47,17 @@ import frc.robot.subsystems.Intake.IntakeIOSpark;
  */
 public class RobotContainer {
   // Subsystems
-  // private final Hopper hopper;
+  private final Hopper hopper;
   private final Intake intake;
-  // private final ClimberSubsystem climberSubsystem;
+  private final ClimberSubsystem climberSubsystem;
+  private final Drive drive;
+  //private final Vision vision;
 
   // Controller
   private final CommandXboxController controller = new CommandXboxController(0);
 
   // Dashboard inputs
+  private final LoggedDashboardChooser<Command> autoChooser;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -40,96 +66,99 @@ public class RobotContainer {
         // Real robot, instantiate hardware IO implementations
         // ModuleIOTalonFX is intended for modules with TalonFX drive, TalonFX turn, and
         // a CANcoder
-        /*  drive =
-        new Drive(
-            new GyroIOPigeon2(),
-            new ModuleIOTalonFX(TunerConstants.FrontLeft),
-            new ModuleIOTalonFX(TunerConstants.FrontRight),
-            new ModuleIOTalonFX(TunerConstants.BackLeft),
-            new ModuleIOTalonFX(TunerConstants.BackRight));*/
-        /*vision =
-        new Vision(
-            drive::addVisionMeasurement,
-            new VisionIOLimelight(camera0Name, drive::getRotation),
-            new VisionIOLimelight(camera1Name, drive::getRotation),
-            new VisionIOLimelight(camera2Name, drive::getRotation),
-            new VisionIOLimelight(camera3Name, drive::getRotation));
-            */
-        // hopper =
-        //     new Hopper(
-        //         new HopperIOSparkMax(Constants.Hopper.FeedCanId, Constants.Hopper.SpindexCanId));
+        drive =
+            new Drive(
+                null,
+                new GyroIOPigeon2(),
+                new ModuleIOTalonFX(TunerConstants.FrontLeft),
+                new ModuleIOTalonFX(TunerConstants.FrontRight),
+                new ModuleIOTalonFX(TunerConstants.BackLeft),
+                new ModuleIOTalonFX(TunerConstants.BackRight));
+        //vision =
+        //    new Vision(
+        //        drive::addVisionMeasurement,
+        //        new VisionIOLimelight(camera0Name, drive::getRotation),
+        //        new VisionIOLimelight(camera1Name, drive::getRotation),
+        //        new VisionIOLimelight(camera2Name, drive::getRotation),
+        //        new VisionIOLimelight(camera3Name, drive::getRotation));
+
+        hopper =
+            new Hopper(
+                new HopperIOSparkMax(Constants.Hopper.FeedCanId, Constants.Hopper.SpindexCanId));
         intake = new Intake(new IntakeIOSpark());
-        // climberSubsystem =
-        //    new ClimberSubsystem(
-        //        new ClimberIOSparkMax(Constants.ClimberConstants.ClimberMotorCANId, 5, 6));
+        climberSubsystem =
+            new ClimberSubsystem(
+                new ClimberIOSparkMax(Constants.ClimberConstants.ClimberMotorCANId, 5, 6));
         break;
 
       case SIM:
         // Sim robot, instantiate physics sim IO implementations
-        /*drive =
-        new Drive(
-            new GyroIO() {},
-            new ModuleIOSim(TunerConstants.FrontLeft),
-            new ModuleIOSim(TunerConstants.FrontRight),
-            new ModuleIOSim(TunerConstants.BackLeft),
-            new ModuleIOSim(TunerConstants.BackRight));
-            */
-        /*  vision =
-        new Vision(
-            drive::addVisionMeasurement,
-            new VisionIOPhotonVisionSim(camera0Name, robotToCamera0, drive::getPose),
-            new VisionIOPhotonVisionSim(camera1Name, robotToCamera1, drive::getPose),
-            new VisionIOPhotonVisionSim(camera2Name, robotToCamera2, drive::getPose),
-            new VisionIOPhotonVisionSim(camera3Name, robotToCamera3, drive::getPose));
-            */
-        // hopper = new Hopper(new HopperIO() {});
+        drive =
+            new Drive(
+                null,
+                new GyroIOPigeon2(),
+                new ModuleIOSim(TunerConstants.FrontLeft),
+                new ModuleIOSim(TunerConstants.FrontRight),
+                new ModuleIOSim(TunerConstants.BackLeft),
+                new ModuleIOSim(TunerConstants.BackRight));
+
+        //vision =
+        //    new Vision(
+        //        drive::addVisionMeasurement,
+        //        new VisionIOPhotonVisionSim(camera0Name, robotToCamera0, drive::getPose),
+        //        new VisionIOPhotonVisionSim(camera1Name, robotToCamera1, drive::getPose),
+        //        new VisionIOPhotonVisionSim(camera2Name, robotToCamera2, drive::getPose),
+        //        new VisionIOPhotonVisionSim(camera3Name, robotToCamera3, drive::getPose));
+
+        hopper = new Hopper(new HopperIO() {});
         intake = new Intake(new IntakeIO() {});
-        // climberSubsystem = new ClimberSubsystem(new ClimberIO() {});
+        climberSubsystem = new ClimberSubsystem(new ClimberIO() {});
         break;
 
       default:
         // Replayed robot, disable IO implementations
-        /* drive =
-        new Drive(
-            new GyroIO() {},
-            new ModuleIO() {},
-            new ModuleIO() {},
-            new ModuleIO() {},
-            new ModuleIO() {});
-            */
-        /* vision =
-        new Vision(
-            drive::addVisionMeasurement,
-            new VisionIO() {},
-            new VisionIO() {},
-            new VisionIO() {},
-            new VisionIO() {});
-            */
-        // hopper = new Hopper(new HopperIO() {});
+        drive =
+            new Drive(
+                null,
+                new GyroIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {});
+
+        //vision =
+        //    new Vision(
+        //        drive::addVisionMeasurement,
+        //        new VisionIO() {},
+        //        new VisionIO() {},
+        //        new VisionIO() {},
+        //        new VisionIO() {});
+
+        hopper = new Hopper(new HopperIO() {});
         intake = new Intake(new IntakeIO() {});
-        // climberSubsystem = new ClimberSubsystem(new ClimberIO() {});
+        climberSubsystem = new ClimberSubsystem(new ClimberIO() {});
         break;
     }
 
     // Set up auto routines
-    /*autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());*/
+    autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
 
-    /* // Set up SysId routines
-        autoChooser.addOption(
-            "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
-        autoChooser.addOption(
-            "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
-        autoChooser.addOption(
-            "Drive SysId (Quasistatic Forward)",
-            drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-        autoChooser.addOption(
-            "Drive SysId (Quasistatic Reverse)",
-            drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-        autoChooser.addOption(
-            "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
-        autoChooser.addOption(
-            "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
-    */
+    // Set up SysId routines
+    autoChooser.addOption(
+        "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
+    autoChooser.addOption(
+        "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
+    autoChooser.addOption(
+        "Drive SysId (Quasistatic Forward)",
+        drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+    autoChooser.addOption(
+        "Drive SysId (Quasistatic Reverse)",
+        drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+    autoChooser.addOption(
+        "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
+    autoChooser.addOption(
+        "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+
     // Configure the button bindings
     configureButtonBindings();
   }
@@ -142,31 +171,29 @@ public class RobotContainer {
    */
   private void configureButtonBindings() {
     // Default command, normal field-relative drive
-    /*  drive.setDefaultCommand(
-    DriveCommands.joystickDrive(
-        drive,
-        () -> -controller.getLeftY(),
-        () -> -controller.getLeftX(),
-        () -> -controller.getRightX()));
-        */
-
-    // Lock to 0° when A button is held
-    /*  controller
-    .a()
-    .whileTrue(
-        DriveCommands.joystickDriveAtAngle(
+    drive.setDefaultCommand(
+        DriveCommands.joystickDrive(
             drive,
             () -> -controller.getLeftY(),
             () -> -controller.getLeftX(),
-            () -> Rotation2d.kZero));
-             */
+            () -> -controller.getRightX()));
+
+    // Lock to 0° when A button is held
+    controller
+        .a()
+        .whileTrue(
+            DriveCommands.joystickDriveAtAngle(
+                drive,
+                () -> -controller.getLeftY(),
+                () -> -controller.getLeftX(),
+                () -> Rotation2d.kZero));
 
     // Switch to X pattern when X button is pressed
-    /* controller.x().onTrue(Commands.runOnce(drive::stopWithX, drive));*/
-    // controller.leftBumper().whileTrue((climberSubsystem.climberRetract()));
-    // controller.rightBumper().whileTrue((climberSubsystem.climberExtend()));
+    controller.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
+    controller.leftBumper().whileTrue((climberSubsystem.climberRetract()));
+    controller.rightBumper().whileTrue((climberSubsystem.climberExtend()));
 
-    /*   // Reset gyro to 0° when B button is pressed
+    // Reset gyro to 0° when B button is pressed
     controller
         .b()
         .onTrue(
@@ -176,7 +203,7 @@ public class RobotContainer {
                             new Pose2d(drive.getPose().getTranslation(), Rotation2d.kZero)),
                     drive)
                 .ignoringDisable(true));
-                */
+
     controller.y().onTrue(intake.togglePosition());
   }
 
@@ -185,7 +212,7 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  /*public Command getAutonomousCommand() {
+  public Command getAutonomousCommand() {
     return autoChooser.get();
-  }*/
+  }
 }
