271e1b0c67184af8b631c1ea5db50ea331f5632d
Rocketdudes4442 <carl.stelly@outlook.com>
2026-02-23 19:28:32 -0600

    I Did something with ff and setReference, barely any idea what im doing

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.Intake;
 
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -11,8 +13,17 @@ public class Intake extends SubsystemBase {
   private final IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
   public boolean FlipFlop = false;
 
+  public TrapezoidProfile.State mCurrentState;
+  public TrapezoidProfile.State mDesiredState;
+  public TrapezoidProfile mTrapezoidProfile;
+
+  public double mReference; // ???? why was this called a State? it a double, not a State
+  public Constraints mConstraints;
+
   public Intake(IntakeIO io) {
     this.io = io;
+    mConstraints = new Constraints(Constants.Intake.MaxV, Constants.Intake.MaxA);
+    mTrapezoidProfile = new TrapezoidProfile(mConstraints);
   }
 
   @Override
@@ -25,6 +36,7 @@ public class Intake extends SubsystemBase {
     Logger.recordOutput("Intake/Sim/", inputs.MovementMotorRPM);
     Logger.recordOutput("Intake/Sim/", inputs.MovementMotorRotation);
 
+    io.ffCommand(inputs.positionRad);
     if (inputs.positionRad > Constants.Intake.RotatoThresholdRAD) {
       io.runRotato(0);
     } else {
@@ -59,5 +71,10 @@ public class Intake extends SubsystemBase {
             FlipFlop = true;
           }
         });
+  }
+  public void SetReference(double position) {
+    mReference = position;
+    mDesiredState = new TrapezoidProfile.State(mReference, 0);
+    Logger.recordOutput("Elevator/Commanded Position", position);
   }
 }
