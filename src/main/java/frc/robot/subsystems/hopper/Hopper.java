8a503d50c6be77ca6d9808a69df07bd822db0c0d
Rocketdudes4442 <carl.stelly@outlook.com>
2026-03-12 17:56:07 -0500

    did fix on the right branch this time

--- src/main/java/frc/robot/subsystems/hopper/Hopper.java
+++ src/main/java/frc/robot/subsystems/hopper/Hopper.java
@@ -79,7 +79,7 @@ public class Hopper extends SubsystemBase {
 
   public Command testSpindex() {
     return run(() -> {
-          io.SpindexSpeedCommand(Constants.Hopper.TestFeedSpeed);
+          io.SpindexSpeedCommand(Constants.Hopper.TestSpindexSpeed);
         })
         .finallyDo(() -> io.SpindexSpeedCommand(0));
   }
