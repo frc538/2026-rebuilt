efb088c544e81b89193991b43d8ef35f4f7f3136
Nick <npatrick04@gmail.com>
2026-03-13 17:40:16 -0500

    limit accel for turret

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -121,10 +121,10 @@ public final class Constants {
     public static final double kFlywheelMomentOfInertia = 0.0004926; // kg * m^2
     public static final double kWheelRadius = 0.050165; // m
 
-    public static final double minrange = -(3 * Math.PI) / 4;
+    public static final double minrange = -(3 * Math.PI) / 4; 
     public static final double maxrange = (3 * Math.PI) / 4;
     public static final double maxV = 1;
-    public static final double maxA = 5;
+    public static final double maxA = 0.5;
 
     public static double allowedClosedLoopError = Math.PI / 6;
 
