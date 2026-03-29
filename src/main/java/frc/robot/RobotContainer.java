c8b84282568e48f9bc8769bfc3b8159806d04b82
Nick Patrick <npatrick04@gmail.com>
2026-03-29 16:26:14 -0500

    Auto events magnolia (#44) (#48)
    * Removed Most of the Autos and added commands for them.
    
    * Added event triggers for the launcher, intake, and climber in pathplanner.
    
    * Added Autos to selectively stay on the side they start on while also fixing the event markers (partially).
    
    Co-authored-by: AspenTheLeaf <145722959+AspenTheLeaf@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,7 @@ package frc.robot;
 import static frc.robot.subsystems.vision.VisionConstants.*;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -198,6 +199,11 @@ public class RobotContainer {
         climberSubsystem = new ClimberSubsystem(new ClimberIO() {});
         break;
     }
+    new NamedCommands().registerCommand("shoot toggle", launcher.toggleShoot());
+    new NamedCommands().registerCommand("intake open", intake.forceIntake());
+    new NamedCommands().registerCommand("intake close", intake.forceIntake());
+    new NamedCommands().registerCommand("climb", climberSubsystem.climberExtend());
+
     nav2 = new Navigation2(drive);
 
     // SmartDashboard.putData(navSys.m_field);
