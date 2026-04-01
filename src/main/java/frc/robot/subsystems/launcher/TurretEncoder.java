c2d698880c6139cb3d5f67a9e5b0231649083cda
AspenTheLeaf <brooklynnwood@eagles.acs-k12.org>
2026-03-31 18:37:19 -0800

    Added stuff to the calibrate command.

--- src/main/java/frc/robot/subsystems/launcher/TurretEncoder.java
+++ src/main/java/frc/robot/subsystems/launcher/TurretEncoder.java
@@ -16,15 +16,16 @@ public class TurretEncoder {
   CANcoder can1;
   CANcoder can2;
 
-  CANcoderConfigurator can1Config;
-  CANcoderConfigurator can2Config;
+  MagnetSensorConfigs can1MagnetSensorConfigs;
+  MagnetSensorConfigs can2MagnetSensorConfigs;
 
   public TurretEncoder() {
     can1 = new CANcoder(Constants.CanIds.turretEncoder1);
     can2 = new CANcoder(Constants.CanIds.turretEncoder2);
+    
 
-    //can1Config = can1.getConfigurator().apply(new MagnetSensorConfigs().withAbsoluteSensorDiscontinuityPoint(1));
-    can2Config = can2.getConfigurator();
+    can1.getConfigurator().refresh(can1MagnetSensorConfigs);
+    can2.getConfigurator().refresh(can2MagnetSensorConfigs);
   }
 
   double read() {
@@ -53,7 +54,7 @@ public class TurretEncoder {
         if (Math.abs(a1 - a2) < Constants.turretConstants.minDistance) {
           Logger.recordOutput("Turret/encoder read valid", true);
           Logger.recordOutput("Turret/position", (a1 + a2) / 2);
-          return (a1 + a2) / 2;
+          return (a1 + a2) / 2; //Subtract 1.38 turns
         }
       }
     }
@@ -62,11 +63,29 @@ public class TurretEncoder {
   }
 
   public Command calibrate() {
-    //double encoder1Rotations = can1.getAbsolutePosition().getValueAsDouble();
-    //double encoder2Rotations = can2.getAbsolutePosition().getValueAsDouble();
+    double encoder1Rotations = can1.getAbsolutePosition().getValueAsDouble();
+    double encoder2Rotations = can2.getAbsolutePosition().getValueAsDouble();
 
-    can1.getConfigurator().apply(new MagnetSensorConfigs().withMagnetOffset(0).withAbsoluteSensorDiscontinuityPoint(1.0));
-    can2.getConfigurator().apply(new MagnetSensorConfigs().withMagnetOffset(180).withAbsoluteSensorDiscontinuityPoint(1.0));
+    // double encoder1Offset = can1.getConfigurator().refresh(can1Config);
+    double EncoderRaw = encoder1Rotations - can1MagnetSensorConfigs.MagnetOffset;
+    double newEncoder1Offset = 0 - EncoderRaw;
+
+    if(newEncoder1Offset < 0) {
+      newEncoder1Offset = newEncoder1Offset + 1;
+    }
+
+    EncoderRaw = encoder2Rotations - can2MagnetSensorConfigs.MagnetOffset;
+    double newEncoder2Offset = 0.5 - EncoderRaw;
+
+    if(newEncoder2Offset < 0) {
+      newEncoder2Offset = newEncoder2Offset + 1;
+    }
+
+    can1MagnetSensorConfigs.MagnetOffset = newEncoder1Offset;
+    can2MagnetSensorConfigs.MagnetOffset = newEncoder2Offset;
+
+    can1.getConfigurator().apply(can1MagnetSensorConfigs);
+    can2.getConfigurator().apply(can2MagnetSensorConfigs);
 
     //System.out.printf("Encoder 1 Bias = %f\n", - encoder1Rotations);
     //System.out.printf("Encoder 2 Bias = %f\n", 180 - encoder2Rotations);
