fb48f14a8f6d79c50f13cb01bf289ba946330c8b
Rocketdudes4442 <carl.stelly@outlook.com>
2026-04-06 22:21:01 -0500

    Merging old useful stuff (#50)
    * did a thing, need to make it break less
    
    * looks like i got the major important stuff, going to attempt to integrate now
    
    * Done?
    
    * multiplied something by 5
    
    * Update RobotContainer.java
    
    Disable drive drift adjust by default, needs testing, but code needs to be merged to be ready to use.
    
    ---------
    
    Co-authored-by: Nick Patrick <npatrick04@gmail.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -262,6 +262,7 @@ public class RobotContainer {
     // Default command, normal field-relative drive
     drive.setDefaultCommand(
         DriveCommands.joystickDrive(
+          //DriveCommands.driveDriftAccount(
             drive,
             () -> -pilotController.getLeftY(),
             () -> -pilotController.getLeftX(),
