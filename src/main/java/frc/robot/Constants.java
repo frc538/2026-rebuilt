cec5d8ed4c1898aba8becf248260852c2c19a5e1
Nick Patrick <npatrick04@gmail.com>
2026-04-07 18:28:03 -0500

    Intake default off, intake up higher volt, and tweaking the rotato speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -208,7 +208,7 @@ public final class Constants {
   }
 
   public final class Intake2 {
-    public static final double upVoltage = -2; // placeholder value in volts
+    public static final double upVoltage = -3; // placeholder value in volts
     public static final double downVoltage = 0.5; // placeholder value in volts
   }
 
@@ -235,7 +235,7 @@ public final class Constants {
     public static final double ArmkD = 0.0;
 
     public static final double RotatoThresholdRAD = Math.PI / 4.0;
-    public static final double RotatoRPM = 0.65;
+    public static final double RotatoRPM = 0.60;
 
     public static final double testRotatoRPM = .2;
 
