38c821ad8fbef02788df654a125a53c7e5576370
Nick Patrick <npatrick04@gmail.com>
2026-04-10 15:07:10 -0500

    Closed loop ramp rate to limit drive spikes

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -60,7 +60,15 @@ public class TunerConstants {
 
   // Initial configs for the drive and steer motors and the azimuth encoder; these cannot be null.
   // Some configs will be overwritten; check the `with*InitialConfigs()` API documentation.
-  private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration();
+  private static final TalonFXConfiguration driveInitialConfigs = 
+    new TalonFXConfiguration()
+    .withClosedLoopRamps(
+        new ClosedLoopRampsConfigs()
+            .withVoltageClosedLoopRampPeriod(0.01));
+    // .withCurrentLimits(
+    //     new CurrentLimitsConfigs()
+    //         .withStatorCurrentLimit(Amps.of(100))
+    //         .withStatorCurrentLimitEnable(true));
   private static final TalonFXConfiguration steerInitialConfigs =
       new TalonFXConfiguration()
           .withCurrentLimits(
